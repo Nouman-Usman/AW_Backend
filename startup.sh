254db662ee6f47b0cd063726170de6bce67b093f
Nouman Usman <2022CS49@student.uet.edu.pk>
2024-12-26 14:42:07 +0500

    Refactor deployment package creation and update startup script to use wsgi.py

--- startup.sh
+++ startup.sh
@@ -3,4 +3,4 @@ cd /home/site/wwwroot
 export PORT=${PORT:-8000}
 export PYTHONPATH=/home/site/wwwroot
 python -m pip install -r requirements.txt
-exec gunicorn --bind=0.0.0.0:$PORT --timeout 600 --workers 4 app:app
+exec gunicorn --bind=0.0.0.0:$PORT wsgi:app
